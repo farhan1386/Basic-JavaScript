d044fec14f50cc058c7dac716559136e39048a52
Farhan Ahmed <farhan1086@gmail.com>
2020-09-25 23:56:07 +0530

    loop in javascript

---
+++ 4. Control Flow/DoWhileLoop.js
@@ -0,0 +1,12 @@
+// The do/while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true, then it will repeat the loop as long as the condition is true.
+let i=0;
+do{
+    console.log(i);
+    i++;
+}while(i<=10)
+
+let i=1;
+do{
+    console.log(i*2);
+    i++;
+}while(i<=10)
